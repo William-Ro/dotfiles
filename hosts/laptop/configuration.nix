72318181128567865f02e6dcc27d2debfee5604a
William-Ro <william.r2409@gmail.com>
2025-03-14 16:58:15 -0600

    common settings to nix-core

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -189,7 +189,4 @@
 
   # Add ability to used TouchID for sudo authentication
   security.pam.services.sudo_local.touchIdAuth = true;
-
-  # Set your time zone.
-  time.timeZone = "America/Costa_Rica";
 }
