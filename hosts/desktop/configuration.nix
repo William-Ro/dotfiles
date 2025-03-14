546e8da880d1024680615f3b02f7259f7ab386e3
William-Ro <william.r2409@gmail.com>
2025-03-14 17:06:19 -0600

    configuration.nix organized for clarity

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -1,38 +1,26 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   config,
   pkgs,
   ...
 }: {
   imports = [
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
+    ./hardware-configuration.nix # Include hardware scan results
   ];
 
-  # Bootloader.
+  # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  system.activationScripts.script.text = ''
-    mkdir -p /var/lib/AccountsService/{icons,users}
-    cp home/deishuu/Pictures/face.png /var/lib/AccountsService/icons/deishuu
-    echo -e "[User]\nIcon=/var/lib/AccountsService/icons/deishuu\n" > /var/lib/AccountsService/users/deishuu
-  '';
+  # Networking
+  networking = {
+    networkmanager.enable = true;
+    # wireless.enable = true; # Uncomment if using Wi-Fi
+    # proxy.default = "http://user:password@proxy:port/";
+    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  };
 
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Select internationalisation properties.
+  # Internationalization
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "es_CR.UTF-8";
     LC_IDENTIFICATION = "es_CR.UTF-8";
@@ -45,76 +33,55 @@
     LC_TIME = "es_CR.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
+  # X11 & Desktop Environment
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
+  # Sound (PipeWire)
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    # jack.enable = true; # Uncomment if using JACK applications
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  # Printing
+  services.printing.enable = true;
 
-  # Install firefox.
+  # Programs
   programs.firefox.enable = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  #environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  #  git
-  #];
+  # System Packages
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+  ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  # System User Icon Setup
+  system.activationScripts.userIcon.text = ''
+    mkdir -p /var/lib/AccountsService/{icons,users}
+    cp /home/deishuu/Pictures/face.png /var/lib/AccountsService/icons/deishuu
+    echo -e "[User]\nIcon=/var/lib/AccountsService/icons/deishuu\n" > /var/lib/AccountsService/users/deishuu
+  '';
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
+  # OpenSSH (Optional)
   # services.openssh.enable = true;
 
-  # Open ports in the firewall.
+  # Firewall Configuration (Optional)
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  # networking.firewall.enable = false; # Disable firewall (not recommended)
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  # NixOS System Version
+  system.stateVersion = "24.11"; # Keep this at initial install version
 }
--- hosts/nix-core.nix
+++ hosts/nix-core.nix
@@ -32,6 +32,7 @@
   # Create /etc/zshrc that loads the nix-darwin environment.
   # this is required if you want to use darwin's default shell - zsh
   programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
   environment.shells = [
     pkgs.zsh
   ];
