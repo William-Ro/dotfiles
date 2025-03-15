a1ddf9e9ca3483adbcaa0a5f41c67cc62a9ae909
William-Ro <william.r2409@gmail.com>
2025-03-14 19:31:15 -0600

    users.default.shell is not available on nix-darwin

--- hosts/nix-core.nix
+++ hosts/nix-core.nix
@@ -32,7 +32,6 @@
   # Create /etc/zshrc that loads the nix-darwin environment.
   # this is required if you want to use darwin's default shell - zsh
   programs.zsh.enable = true;
-  users.defaultUserShell = pkgs.zsh;
   environment.shells = [
     pkgs.zsh
   ];
