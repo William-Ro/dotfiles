a422a5b61c275ff001095f0afa84ebc2871949a6
William-Ro <william.r2409@gmail.com>
2025-03-30 16:03:17 -0600

    chore: add env

--- modules/linux/hyprland.nix
+++ modules/linux/hyprland.nix
@@ -7,25 +7,12 @@
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
-      "$mod" = "SUPER";
-      bind =
-        [
-          "$mod, F, exec, firefox"
-          ", Print, exec, grimblast copy area"
-        ]
-        ++ (
-          # workspaces
-          # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-          builtins.concatLists (builtins.genList (
-              i: let
-                ws = i + 1;
-              in [
-                "$mod, code:1${toString i}, workspace, ${toString ws}"
-                "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-              ]
-            )
-            9)
-        );
+      env = [
+        "NIXOS_OZONE_WL, 1"
+        "NIXPKGS_ALLOW_UNFREE, 1"
+        "WLR_NO_HARDWARE_CURSORS, 1"
+        "EDITOR,nvim"
+      ];
     };
   };
 }
