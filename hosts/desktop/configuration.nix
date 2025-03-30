7085984c24f4f43c6dd9df3feac609b2ba5947c4
William-Ro <william.r2409@gmail.com>
2025-03-30 13:38:09 -0600

    chore: add comments

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -19,6 +19,8 @@
     # proxy.default = "http://user:password@proxy:port/";
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
   };
+
+  # Bluetooth
   hardware.bluetooth.enable = true;
 
   # Internationalization
