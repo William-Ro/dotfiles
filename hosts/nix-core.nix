b2c0cfa3162915fd1f48f3fc5b5fbd3457abcbca
William-Ro <william.r2409@gmail.com>
2025-03-30 23:08:17 -0600

    chore: disable auto-optimise-store

--- hosts/nix-core.nix
+++ hosts/nix-core.nix
@@ -66,6 +66,6 @@
   #   https://github.com/NixOS/nix/issues/7273
   # "error: cannot link '/nix/store/.tmp-link-xxxxx-xxxxx' to '/nix/store/.links/xxxx': File exists"
   nix.settings = {
-    auto-optimise-store = true;
+    auto-optimise-store = false;
   };
 }
