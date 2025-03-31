c87f24e679f02bc34497e46c32a921e05c2255e3
William-Ro <william.r2409@gmail.com>
2025-03-30 18:06:39 -0600

    chore: update ghostty module

--- modules/linux/ghostty.nix
+++ modules/linux/ghostty.nix
@@ -1,30 +1,20 @@
 {pkgs, ...}: {
   programs.ghostty = {
     enable = true;
-    package = pkgs.ghostty;
   };
   home.file."./.config/ghostty/config".text = ''
-
-    #theme = Aura
     theme = Dracula
-    #theme = Aardvark Blue
-    #theme = GruvboxDarkHard
     adjust-cell-height = 10%
     window-theme = dark
-    window-height = 32
+    window-height = 20
     window-width = 110
-    background-opacity = 0.95
+    background-opacity = 1
     background-blur-radius = 60
     selection-background = #2d3f76
     selection-foreground = #c8d3f5
     cursor-style = bar
     mouse-hide-while-typing = true
 
-    # keybindings
-    keybind = alt+s>r=reload_config
-    keybind = alt+s>x=close_surface
-
-    keybind = alt+s>n=new_window
 
     # tabs
     keybind = alt+s>c=new_tab
@@ -60,17 +50,13 @@
     # other
     #copy-on-select = clipboard
 
-    font-size = 12
-    font-family = JetBrainsMono Nerd Font Mono
-    #font-family-bold = JetBrainsMono NFM Bold
-    #font-family-bold-italic = JetBrainsMono NFM Bold Italic
-    #font-family-italic = JetBrainsMono NFM Italic
+    font-size = 22
+    font-family = JetBrainsMono Nerd Font
+    font-family-bold = JetBrainsMono NFM Bold
+    font-family-bold-italic = JetBrainsMono NFM Bold Italic
+    font-family-italic = JetBrainsMono NFM Italic
 
-    #font-family = BerkeleyMono Nerd Font
-    #font-family = Iosevka Nerd Font
-    # font-family = SFMono Nerd Font
-
-    title = "Ghostty"
+    title = "Deishuu's Ghostty"
 
     wait-after-command = false
     shell-integration = detect
