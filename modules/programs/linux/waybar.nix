138bad462fded167e78432295d9e3548f003e792
William-Ro <william.r2409@gmail.com>
2025-04-01 11:48:34 -0600

    chore: clean unused features waybar

--- modules/programs/linux/waybar.nix
+++ modules/programs/linux/waybar.nix
@@ -11,133 +11,90 @@
       target = "graphical-session.target";
     };
     style = ''
-             * {
-               font-family: "JetBrainsMono Nerd Font";
-               font-size: 12pt;
-               font-weight: bold;
-               border-radius: 8px;
-               transition-property: background-color;
-               transition-duration: 0.5s;
-             }
-             window#waybar {
-               background-color: transparent;
-             }
-             window > box {
-               margin-left: 5px;
-               margin-right: 5px;
-               margin-top: 5px;
-               background-color: #232323;
-               padding: 3px;
-               padding-left:8px;
-               border: 2px none #d9e0ee;
-             }
-             tooltip {
-               background: #232323;
-             }
-             tooltip label {
-               color: #d9e0ee;
-             }
-       #custom-launcher {
-               font-size: 20px;
-               padding-left: 8px;
-               padding-right: 6px;
-               color: #d9e0ee;
-             }
-       #mode, #clock, #memory, #temperature,#cpu,#mpd, #custom-wall, #temperature, #backlight, #pulseaudio, #network, #battery, #custom-powermenu, #custom-cava-internal {
-               padding-left: 10px;
-               padding-right: 10px;
-             }
-             /* #mode { */
-             /* 	margin-left: 10px; */
-             /* 	background-color: rgb(248, 189, 150); */
-             /*     color: rgb(26, 24, 38); */
-             /* } */
-       #memory {
-               color: rgb(181, 232, 224);
-             }
-       #cpu {
-               color: rgb(245, 194, 231);
-             }
-       #clock {
-               color: rgb(217, 224, 238);
-             }
-      /* #idle_inhibitor {
-               color: rgb(221, 182, 242);
-             }*/
-       #custom-wall {
-               color: #33ccff;
-          }
-       #temperature {
-               color: rgb(150, 205, 251);
-             }
-       #backlight {
-               color: rgb(248, 189, 150);
-             }
-       #pulseaudio {
-               color: rgb(245, 224, 220);
-             }
-       #network {
-               color: #ABE9B3;
-             }
-       #network.disconnected {
-               color: rgb(255, 255, 255);
-             }
-       #custom-powermenu {
-               color: rgb(242, 143, 173);
-               padding-right: 8px;
-             }
-       #tray {
-               padding-right: 8px;
-               padding-left: 10px;
-             }
-       #mpd.paused {
-               color: #414868;
-               font-style: italic;
-             }
-       #mpd.stopped {
-               background: transparent;
-             }
-       #mpd {
-               color: #c0caf5;
-             }
-       #custom-cava-internal{
-               font-family: "Hack Nerd Font" ;
-               color: #33ccff;
-             }
+            * {
+              font-family: "JetBrainsMono Nerd Font";
+              font-size: 12pt;
+              font-weight: bold;
+              border-radius: 8px;
+              transition-property: background-color;
+              transition-duration: 0.5s;
+            }
+            window#waybar {
+              background-color: transparent;
+            }
+            window > box {
+              margin-left: 5px;
+              margin-right: 5px;
+              margin-top: 5px;
+              background-color: #232323;
+              padding: 3px;
+              padding-left:8px;
+              border: 2px none #d9e0ee;
+            }
+            tooltip {
+              background: #232323;
+            }
+            tooltip label {
+              color: #d9e0ee;
+            }
+      #custom-nix-icon {
+              font-size: 20px;
+              color: #d9e0ee;
+            }
+      #clock {
+              color: rgb(217, 224, 238);
+            }
+      #pulseaudio {
+              color: rgb(245, 224, 220);
+            }
+      #memory {
+              color: rgb(181, 232, 224);
+            }
+      #cpu {
+              color: rgb(245, 194, 231);
+            }
+      #network {
+              color: #ABE9B3;
+            }
+      #network.disconnected {
+              color: rgb(255, 255, 255);
+            }
+
+      #custom-nix-icon, #clock, #pulseaudio, #memory,#cpu, #network{
+              padding-left: 10px;
+              padding-right: 10px;
+            }
     '';
     settings = [
       {
         "layer" = "top";
         "position" = "top";
         modules-left = [
-          "custom/launcher"
-          "temperature"
-          "mpd"
-          "custom/cava-internal"
+          "custom/nix-icon"
         ];
         modules-center = [
           "clock"
         ];
         modules-right = [
           "pulseaudio"
-          "backlight"
           "memory"
           "cpu"
           "network"
-          "custom/powermenu"
-          "tray"
         ];
-        "custom/launcher" = {
+        "custom/nix-icon" = {
           "format" = " ";
           "on-click" = "pkill rofi || rofi2";
           "on-click-middle" = "exec wallpaper_default";
           "on-click-right" = "exec wallpaper_random";
           "tooltip" = false;
         };
-        "custom/cava-internal" = {
-          "exec" = "sleep 1s && cava-internal";
-          "tooltip" = false;
+        "clock" = {
+          "interval" = 1;
+          "format" = "{:%I:%M %p  %A %b %d}";
+          "tooltip" = true;
+          "tooltip-format" = "{:%A; %d %B %Y}\n<tt>{calendar}</tt>";
         };
+
         "pulseaudio" = {
           "scroll-step" = 1;
           "format" = "{icon} {volume}%";
@@ -148,12 +105,7 @@
           "on-click" = "pamixer -t";
           "tooltip" = false;
         };
-        "clock" = {
-          "interval" = 1;
-          "format" = "{:%I:%M %p  %A %b %d}";
-          "tooltip" = true;
-          "tooltip-format" = "{:%A; %d %B %Y}\n<tt>{calendar}</tt>";
-        };
+
         "memory" = {
           "interval" = 1;
           "format" = "󰻠 {percentage}%";
@@ -165,36 +117,13 @@
           "interval" = 1;
           "format" = "󰍛 {usage}%";
         };
-        "mpd" = {
-          "max-length" = 25;
-          "format" = "<span foreground='#bb9af7'></span> {title}";
-          "format-paused" = " {title}";
-          "format-stopped" = "<span foreground='#bb9af7'></span>";
-          "format-disconnected" = "";
-          "on-click" = "mpc --quiet toggle";
-          "on-click-right" = "mpc update; mpc ls | mpc add";
-          "on-click-middle" = "kitty --class='ncmpcpp' ncmpcpp ";
-          "on-scroll-up" = "mpc --quiet prev";
-          "on-scroll-down" = "mpc --quiet next";
-          "smooth-scrolling-threshold" = 5;
-          "tooltip-format" = "{title} - {artist} ({elapsedTime:%M:%S}/{totalTime:%H:%M:%S})";
-        };
         "network" = {
           "format-disconnected" = "󰯡 Disconnected";
-          "format-ethernet" = "󰒢 Connected!";
+          "format-ethernet" = "󰒢 Connected";
           "format-linked" = "󰖪 {essid} (No IP)";
           "format-wifi" = "󰖩 {essid}";
           "interval" = 1;
           "tooltip" = false;
-        };
-        "custom/powermenu" = {
-          "format" = "";
-          "on-click" = "pkill rofi || ~/.config/rofi/powermenu/type-3/powermenu.sh";
-          "tooltip" = false;
-        };
-        "tray" = {
-          "icon-size" = 15;
-          "spacing" = 5;
         };
       }
     ];
