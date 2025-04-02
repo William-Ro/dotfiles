aa2540978f90b8075728caf85fbe6d9304e71990
William-Ro <william.r2409@gmail.com>
2025-04-02 09:57:18 -0600

    chore: update nwg-look module

--- modules/linux/nwg-look.nix
+++ modules/linux/nwg-look.nix
@@ -4,7 +4,47 @@
   pkgs,
   ...
 }: {
-  programs.nwg-look = {
+  home.packages = with pkgs; [
+    nwg-look
+  ];
+
+  gtk = {
     enable = true;
+    theme = {
+      name = "Graphite-Dark";
+      package = pkgs.graphite-gtk-theme.override {
+        tweaks = ["darker"];
+        themeVariants = ["default"];
+        colorVariants = ["dark"];
+      };
+    };
+    cursorTheme = {
+      name = "Bibata-Modern-Classic";
+      package = pkgs.bibata-cursors;
+      size = 23;
+    };
+    font = {
+      name = "JetBrainsMono Nerd Font";
+      size = 11;
+    };
+
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
   };
+
+  xdg.configFile."xsettingsd/xsettingsd.conf".text = ''
+    Net/ThemeName "Graphite-Dark"
+    Net/IconThemeName "Graphite-Dark"
+    Gtk/CursorThemeName "Bibata-Modern-Classic"
+    Net/EnableEventSounds 1
+    EnableInputFeedbackSounds 0
+    Xft/Antialias 1
+    Xft/Hinting 1
+    Xft/HintStyle "hintslight"
+    Xft/RGBA "rgb"
+  '';
 }
