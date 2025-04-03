2602a128dd0da6daa4f571c967b0b400ac67360d
William-Ro <william.r2409@gmail.com>
2025-04-02 21:52:07 -0600

    chore: add comments and delete unused tools

--- modules/linux/core.nix
+++ modules/linux/core.nix
@@ -1,26 +1,31 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    nvidia-vaapi-driver
     spotify
     firefox
-    telegram-desktop
     qbittorrent
-    lm_sensors
-    mpv
-    playerctl
-    mpc
-    killall
-    keychain
-    pamixer
-    # GNOME Applications
-    eog
-    sushi
-    # bluetooth manager
-    overskride
-    # network manager applet
-    networkmanagerapplet
-    # sound manager
-    pavucontrol
+
+    nvidia-vaapi-driver # VA-API driver for NVIDIA GPUs (hardware acceleration)
+
+    # Basic utilities
+    lm_sensors # Hardware monitoring (temperature, voltage, fan speed)
+    mpv # Media player (video & audio)
+    playerctl # Command-line media player controller
+    mpc # CLI client for Music Player Daemon (MPD)
+    killall # Kill processes by name
+    keychain # Manages SSH and GPG keys across sessions
+    pamixer # PulseAudio volume control from the terminal
+
+    # GNOME applications
+    eog # Eye of GNOME - image viewer
+
+    # Bluetooth manager
+    overskride # Bluetooth management tool
+
+    # Network manager applet
+    networkmanagerapplet # GUI for managing network connections
+
+    # Sound manager
+    pavucontrol # Advanced PulseAudio volume control
   ];
   programs = {
   };
