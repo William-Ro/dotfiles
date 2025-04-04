275e4e72b46e69e03f13271b8a9cd1605b5af827
William-Ro <william.r2409@gmail.com>
2025-04-04 14:57:46 -0600

    chore: fix date format

--- modules/linux/waybar.nix
+++ modules/linux/waybar.nix
@@ -104,7 +104,7 @@
         };
         "clock" = {
           "interval" = 1;
-          "format" = "{:%I:%M %p  %A %b %d}";
+          "format" = "{:%I:%M  %A %b %d}";
           "tooltip" = true;
           "tooltip-format" = "{:%A; %d %B %Y}\n<tt>{calendar}</tt>";
         };
