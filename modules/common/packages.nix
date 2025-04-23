dcaed7aabf8997a24854b556ed9c3bf2ad893231
William-Ro <william.r2409@gmail.com>
2025-04-23 17:59:50 +0000

    feat: add fonts

--- modules/common/packages.nix
+++ modules/common/packages.nix
@@ -11,4 +11,23 @@
     curl
     wget
   ];
+  # Fonts
+  fonts = {
+    packages = with pkgs; [
+      # icon fonts
+      material-design-icons
+      font-awesome
+
+      # nerdfonts
+      nerd-fonts.symbols-only
+      nerd-fonts.fira-code
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.iosevka
+
+      noto-fonts
+      noto-fonts-emoji
+      font-awesome_5
+      font-awesome_6
+    ];
+  };
 }
