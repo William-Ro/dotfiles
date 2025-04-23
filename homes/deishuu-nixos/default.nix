1bbd49d2559d9038664da01fbbd0cec0020ec669
William-Ro <william.r2409@gmail.com>
2025-04-23 17:59:40 +0000

    feat: add gtk to nixos

--- homes/deishuu-nixos/default.nix
+++ homes/deishuu-nixos/default.nix
@@ -73,6 +73,31 @@
     };
   };
 
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Graphite-Dark";
+      package = pkgs.graphite-gtk-theme.override {
+        tweaks = ["darker"];
+        themeVariants = ["default"];
+        colorVariants = ["dark"];
+      };
+    };
+    iconTheme = {
+      package = pkgs.tela-icon-theme;
+      name = "Tela-black";
+    };
+    cursorTheme = {
+      name = "Bibata-Modern-Classic";
+      package = pkgs.bibata-cursors;
+      size = 23;
+    };
+    font = {
+      name = "JetBrainsMono Nerd Font";
+      size = 11;
+    };
+  };
+
   programs = {
   };
 
