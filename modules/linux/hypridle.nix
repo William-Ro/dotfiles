8c54816b6f648aa9619be4d64d6a9428a449cb96
William-Ro <william.r2409@gmail.com>
2025-04-28 13:50:13 -0600

    chore: drop brillo

--- modules/linux/hypridle.nix
+++ modules/linux/hypridle.nix
@@ -8,7 +8,6 @@
 in {
   home.packages = with pkgs; [
     hypridle
-    brillo
   ];
   services.hypridle = {
     enable = true;
@@ -18,13 +17,7 @@ in {
         after_sleep_cmd = "hyprctl dispatch dpms on";
         lock_cmd = "hyprlock";
       };
-
       listener = [
-        {
-          timeout = timeout - 10;
-          on-timeout = "brillo -u 500000 -S 10";
-          on-resume = "brillo -I -u 250000";
-        }
         {
           timeout = timeout;
           on-timeout = "hyprctl dispatch dpms off";
