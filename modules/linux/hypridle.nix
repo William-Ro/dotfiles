fc4c0d3bc5c1e0a992976cb24f5359f27499bb26
William-Ro <william.r2409@gmail.com>
2025-04-30 18:36:48 -0600

    chore: fix timeout

--- modules/linux/hypridle.nix
+++ modules/linux/hypridle.nix
@@ -4,20 +4,31 @@
   config,
   ...
 }: let
-  timeout = 100;
-in {
+  timeout = 600;
+
+  lock = "${pkgs.systemd}/bin/loginctl lock-session";
+  brillo = "${pkgs.brillo}/bin/brillo";
   home.packages = with pkgs; [
     hypridle
+    brillo
   ];
+in {
   services.hypridle = {
     enable = true;
+
     settings = {
       general = {
-        before_sleep_cmd = "hyprlock";
+        before_sleep_cmd = lock;
         after_sleep_cmd = "hyprctl dispatch dpms on";
-        lock_cmd = "hyprlock";
+        lock_cmd = "pgrep hyprlock || ${pkgs.hyprlock}/bin/hyprlock";
       };
+
       listener = [
+        {
+          timeout = timeout - 10;
+          on-timeout = "${brillo} -O; ${brillo} -u 500000 -S 10";
+          on-resume = "${brillo} -I -u 250000";
+        }
         {
           timeout = timeout;
           on-timeout = "hyprctl dispatch dpms off";
@@ -25,7 +36,7 @@ in {
         }
         {
           timeout = timeout + 10;
-          on-timeout = "hyprlock";
+          on-timeout = lock;
         }
       ];
     };
