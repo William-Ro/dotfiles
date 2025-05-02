d3ad640478e608012a3f7616ee8cd4e4b5e382bd
William-Ro <william.r2409@gmail.com>
2025-05-02 13:19:45 -0600

    chore: create librewolf module

---
+++ modules/common/librewolf.nix
@@ -0,0 +1,45 @@
+{
+  inputs,
+  pkgs,
+  system,
+  ...
+}: {
+  programs.librewolf = {
+    enable = true;
+
+    profiles.default = {
+      # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.firefox.profiles._name_.containersForce
+      containersForce = true;
+      # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.firefox.profiles._name_.search.force
+      search.force = true;
+
+      extensions = {
+        packages = with inputs.firefox-addons.packages.${pkgs.system}; [
+          bitwarden
+          clearurls
+          new-tab-override
+          darkreader
+          fastforwardteam
+          hover-zoom-plus
+          istilldontcareaboutcookies
+          privacy-badger
+          sponsorblock
+          ublock-origin
+        ];
+      };
+      settings = {
+        "browser.startup.homepage" = "https://william-ro.github.io/startpage/";
+        "media.videocontrols.picture-in-picture.enabled" = false;
+        "media.videocontrols.picture-in-picture.video-toggle.enabled" = false;
+        "ui.key.menuAccessKeyFocuses" = false;
+        "identity.fxaccounts.enabled" = false;
+        "signon.rememberSignons" = false;
+        "browser.toolbars.bookmarks.visibility" = "never";
+        "network.http.referer.XOriginPolicy" = 2;
+        "privacy.trackingprotection.enabled" = true;
+        "dom.security.https_only_mode" = true;
+        "media.peerconnection.enabled" = false;
+      };
+    };
+  };
+}
