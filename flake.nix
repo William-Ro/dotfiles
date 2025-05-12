5d5746d8e27c4c04071ed8b8e149c6b046248bc8
William-Ro <william.r2409@gmail.com>
2025-05-11 21:58:27 -0600

    chore: add matugen input

--- flake.nix
+++ flake.nix
@@ -13,8 +13,14 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    matugen = {
+      url = "github:/InioX/Matugen";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
