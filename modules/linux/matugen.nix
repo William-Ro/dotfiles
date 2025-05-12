abdaa36eb315e68b6660fadd03dbe64ae4c56f93
William-Ro <william.r2409@gmail.com>
2025-05-12 15:02:22 -0600

    feat: add wallpaper to matugen

--- modules/linux/matugen.nix
+++ modules/linux/matugen.nix
@@ -1,14 +1,22 @@
-{inputs, ...}: {
+{
+  inputs,
+  config,
+  ...
+}: {
   imports = [
     inputs.matugen.nixosModules.default
   ];
 
   programs.matugen = {
     enable = true;
+    wallpaper = {
+      # Path to the wallpaper image
+      path = config.wallpaper;
+    };
 
     templates = {
       kittty = {
-        input_path = "./templates/kitty.conf";
+        input_path = "${./templates/kitty-colors.conf}";
         output_path = "~/.config/kitty/colors.conf";
       };
     };
