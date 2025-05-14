44e0113e3cd001c320c209119df332b33c58bf91
William-Ro <william.r2409@gmail.com>
2025-05-13 20:39:03 -0600

    feat: create minecraft server module

---
+++ modules/server/minecraft.nix
@@ -0,0 +1,23 @@
+{inputs, ...}:
+# Make sure the flake inputs are in your system's config
+{
+  imports = [inputs.nix-minecraft.nixosModules.minecraft-servers];
+  nixpkgs.overlays = [inputs.nix-minecraft.overlay];
+
+  # Minecraft server settings
+  services.minecraft-servers = {
+    enable = true;
+    eula = true;
+    openFirewall = true;
+
+    servers = {
+      sergioland = {
+        enable = true;
+        jvmOpts = "-Xmx4G -Xms2G";
+
+        # Specify the custom minecraft server package
+        package = pkgs.minecraftServers.vanilla-server;
+      };
+    };
+  };
+}
