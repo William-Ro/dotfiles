b4cee1fb613f2d2a6984cf6e199213d80307ece5
William-Ro <william.r2409@gmail.com>
2025-05-15 15:06:21 -0600

    chore: use rofi instead of rofi-wayland

--- modules/linux/rofi/default.nix
+++ modules/linux/rofi/default.nix
@@ -1,18 +1,18 @@
 {
-  programs.rofi-wayland = {
+  programs.rofi = {
     enable = true;
     font = "Roboto Mono 12";
     theme = ./rofi-theme.rasi;
 
     extraConfig = {
-      "fixed-num-lines" = "true";
-      "show-icons" = "false";
-      "sidebar-mode" = "false";
-      "scroll-method" = "1";
-      "click-to-exit" = "true";
-      "combi-hide-mode-prefix" = "false";
+      "fixed-num-lines" = true;
+      "show-icons" = false;
+      "sidebar-mode" = false;
+      "scroll-method" = 1;
+      "click-to-exit" = true;
+      "combi-hide-mode-prefix" = false;
       "display-drun" = "\"\"";
-      "disable-history" = "true";
+      "disable-history" = true;
     };
   };
 }
