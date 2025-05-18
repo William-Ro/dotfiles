ce59d3854e289602899c29877110864b2259f9c1
William-Ro <william.r2409@gmail.com>
2025-05-18 17:32:32 -0600

    reformat: add spaces between icons

--- modules/common/starship.nix
+++ modules/common/starship.nix
@@ -11,18 +11,18 @@
       };
 
       character = {
-        success_symbol = "[ ](purple)";
-        error_symbol = "[ ](red)";
-        vimcmd_symbol = "[ ](green)";
+        success_symbol = "[](purple) ";
+        error_symbol = "[](red) ";
+        vimcmd_symbol = "[](green) ";
       };
 
       git_branch = {
-        format = "[$branch ]($style)";
+        format = "[$branch]($style) ";
         style = "bright-black";
       };
 
       git_status = {
-        format = "($ahead_behind$stashed)($style) ";
+        format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style) ";
         style = "cyan";
         conflicted = "​";
         untracked = "​";
