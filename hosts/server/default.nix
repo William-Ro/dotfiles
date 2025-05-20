6753ed0dfbc50b928126710b452fcadd5407f6c0
William-Ro <william.r2409@gmail.com>
2025-05-19 19:19:04 -0600

    feat: create host module for server

---
+++ hosts/server/default.nix
@@ -0,0 +1,76 @@
+{
+  config,
+  inputs,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = lib.imports [
+    ./hardware-configuration.nix
+    "common/nix"
+    "common/packages"
+    "server/minecraft"
+  ];
+
+  # Bootloader
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+      # Disables showing the generations menu, it can be still accessed when holding ‹spacebar› while booting
+      # This makes the boot more "flicker free".
+      timeout = 0;
+    };
+  };
+
+  # Internationalization
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_TIME = "en_US.UTF-8";
+    LC_ADDRESS = "es_CR.UTF-8";
+    LC_IDENTIFICATION = "es_CR.UTF-8";
+    LC_MEASUREMENT = "es_CR.UTF-8";
+    LC_MONETARY = "es_CR.UTF-8";
+    LC_NAME = "es_CR.UTF-8";
+    LC_NUMERIC = "es_CR.UTF-8";
+    LC_PAPER = "es_CR.UTF-8";
+    LC_TELEPHONE = "es_CR.UTF-8";
+  };
+
+  # Networking
+  networking = {
+    networkmanager.enable = true;
+    # wireless.enable = true; # Uncomment if using Wi-Fi
+    # proxy.default = "http://user:password@proxy:port/";
+    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  };
+
+  users.users.${config.username} = {
+    isNormalUser = true;
+    extraGroups = ["networkmanager" "wheel" "docker"];
+    shell = pkgs.zsh;
+  };
+
+  # Docker & Podman
+  virtualisation.docker.enable = true;
+
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
+  # Services
+  services = {
+    openssh = {
+      enable = true;
+    };
+    xserver = {
+      xkb = {
+        layout = "us";
+        variant = "intl";
+      };
+    };
+  };
+
+  # NixOS System Version
+  system.stateVersion = "24.11"; # Keep this at initial install version
+}
