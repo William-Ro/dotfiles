0b465162c601874c10d81c6512c200b5ed4a1ae3
William-Ro <william.r2409@gmail.com>
2025-08-28 21:11:44 -0600

    refactor(desktop.nix): change deprecated definitions

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -49,7 +49,7 @@
   hardware.bluetooth.enable = true;
 
   # Sound (PipeWire)
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   security.polkit.enable = true;
   services.pipewire = {
@@ -70,8 +70,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
