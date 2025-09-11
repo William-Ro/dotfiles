9a0dd301854cc463400dd46bc9df689941316dfd
William-Ro <william.r2409@gmail.com>
2025-09-11 15:42:24 -0600

    chore: drop interactive shell

--- modules/common/fish.nix
+++ modules/common/fish.nix
@@ -46,28 +46,5 @@
       v = "nvim";
       lg = "lazygit";
     };
-
-    interactiveShellInit = ''
-      set -U fish_prompt_pwd_dir_length 10
-      set -g hydro_multiline false
-
-      set -g hydro_symbol_start ""
-      set -g hydro_symbol_prompt "→"
-      set -g hydro_symbol_prompt ""
-      set -g hydro_symbol_error ""
-      set -g hydro_symbol_vim_cmd ""
-
-      set -g hydro_symbol_git_dirty "*"
-      set -g hydro_symbol_git_ahead "⇡"
-      set -g hydro_symbol_git_behind "⇣"
-      set -g hydro_symbol_git_stashed "≡"
-      set -g hydro_color_git brblack
-
-      set -g hydro_color_pwd blue
-      set -g hydro_color_prompt magenta
-      set -g hydro_color_duration yellow
-
-      set -g hydro_cmd_duration_threshold 10000
-    '';
   };
 }
