60197cbc7ccce72a100b23cd7ef6e9b71ae7fff5
William-Ro <william.r2409@gmail.com>
2026-02-06 12:12:46 -0600

    fix: remove XDG portal config from home-manager
    GNOME already provides xdg-desktop-portal at system level
    Duplicate configuration causes conflicts

--- modules/linux/electron.nix
+++ modules/linux/electron.nix
@@ -15,14 +15,4 @@
     --ozone-platform=wayland
     --enable-wayland-ime
   '';
-
-  # XDG Desktop Portal for screensharing support
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gnome
-      xdg-desktop-portal-gtk
-    ];
-    config.common.default = "*";
-  };
 }
