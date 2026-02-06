dcf7cf3cac83900a1ccceeec43aa5712f2d01950
William-Ro <william.r2409@gmail.com>
2026-02-06 12:21:03 -0600

    chore: add wayland support

--- modules/linux/electron.nix
+++ modules/linux/electron.nix
@@ -4,6 +4,10 @@
   lib,
   ...
 }: {
+  home.sessionVariables = {
+    # Enable Wayland support for Electron apps
+    NIXOS_OZONE_WL = "1";
+  };
   # Configure VSCode for Wayland
   xdg.configFile."code-flags.conf".text = ''
     --enable-features=UseOzonePlatform,WaylandWindowDecorations
