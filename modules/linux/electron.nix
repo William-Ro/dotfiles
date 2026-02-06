9a00b95df8276ab4b1b17052732a5056ffb8ffd8
William-Ro <william.r2409@gmail.com>
2026-02-06 12:04:36 -0600

    fix: use shell wrapper for Discord instead of flags file
    Discord doesn't read config files - create wrapper script with Wayland flags

--- modules/linux/electron.nix
+++ modules/linux/electron.nix
@@ -7,17 +7,24 @@
   home.sessionVariables = {
     # Enable Wayland support for Electron apps
     NIXOS_OZONE_WL = "1";
-    
+
     # Electron performance flags
     ELECTRON_OZONE_PLATFORM_HINT = "auto";
   };
 
-  # Install Discord normally - flags will be read from electron-flags.conf
+  # Create Discord wrapper with Wayland flags
   home.packages = [
-    pkgs.discord
+    (pkgs.writeShellScriptBin "discord" ''
+      exec ${pkgs.discord}/bin/discord \
+        --enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer \
+        --ozone-platform=wayland \
+        --enable-gpu-rasterization \
+        --enable-zero-copy \
+        "$@"
+    '')
   ];
 
-  # Configure XDG config for Electron apps
+  # Configure XDG config for VSCode
   xdg.configFile."code-flags.conf".text = ''
     --enable-features=UseOzonePlatform,WaylandWindowDecorations
     --ozone-platform=wayland
@@ -26,17 +33,4 @@
     --enable-zero-copy
   '';
 
-  xdg.configFile."discord-flags.conf".text = ''
-    --enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer
-    --ozone-platform=wayland
-    --enable-gpu-rasterization
-    --enable-zero-copy
-  '';
-
-  xdg.configFile."electron-flags.conf".text = ''
-    --enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer
-    --ozone-platform=wayland
-    --enable-gpu-rasterization
-    --enable-zero-copy
-  '';
-}
+  # Generic Electron apps flags
