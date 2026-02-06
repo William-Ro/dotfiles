17cc7c5eaca620c1836f16ff9fcbadd8235bec96
William-Ro <william.r2409@gmail.com>
2026-02-06 10:07:31 -0600

    chore: exclude more gnome apps

--- modules/linux/default.nix
+++ modules/linux/default.nix
@@ -6,9 +6,17 @@
 }: {
   # Exclude default GNOME apps we don't need
   environment.gnome.excludePackages = with pkgs; [
+    epiphany # GNOME Web browser (you have Firefox and Chromium)
+    gnome-tour # First-run tour
+    gnome-music # Music player (you have mpv)
+    totem # Video player (you have mpv)
+    geary # Email client
     gnome-calendar
+    gnome-contacts
+    gnome-maps
     gnome-weather
     simple-scan # Document scanner
+    yelp # Help viewer
   ];
 
   # Enable GNOME's file indexing and search
