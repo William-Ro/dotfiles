5e8b75ef8cfd58bdcd264d81a0dfdb9d3fd1e45d
William-Ro <william.r2409@gmail.com>
2026-02-14 08:37:58 -0600

    feat: add firefox preferences

--- modules/common/firefox.nix
+++ modules/common/firefox.nix
@@ -6,9 +6,19 @@
 }: {
   programs.firefox = {
     enable = true;
-    package = pkgs.firefox-bin;
+    package = pkgs.firefox;
 
     profiles.default = {
+      settings = {
+        "ui.key.menuAccessKeyFocuses" = false;
+        "signon.rememberSignons" = false;
+        "signon.autofillForms" = false;
+        "signon.generation.enabled" = false;
+        "browser.translations.automaticallyPopup" = false;
+        "browser.translations.enable" = false;
+        "extensions.unifiedExtensions.enabled" = true;
+      };
+
       extensions = {
         packages = with inputs.firefox-addons.packages.${pkgs.stdenv.hostPlatform.system}; [
           bitwarden
