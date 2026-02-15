f9103ce3d79f6d635f915f3f332a0c219096b323
William-Ro <william.r2409@gmail.com>
2026-02-14 23:29:09 -0600

    chore(starship): cleanup preset

--- modules/common/starship.nix
+++ modules/common/starship.nix
@@ -2,50 +2,109 @@
   programs.starship = {
     enable = true;
     settings = {
-      format = ''
-        $username$hostname$directory$git_branch$git_state$git_status$cmd_duration$python$character
-      '';
+      line_break.disabled = true;
+      aws.symbol = " ";
+      buf.symbol = " ";
+      bun.symbol = " ";
+      c.symbol = " ";
+      cpp.symbol = " ";
+      cmake.symbol = " ";
+      conda.symbol = " ";
+      crystal.symbol = " ";
+      dart.symbol = " ";
+      deno.symbol = " ";
+      directory.read_only = " 󰌾";
+      docker_context.symbol = " ";
+      elixir.symbol = " ";
+      elm.symbol = " ";
+      fennel.symbol = " ";
+      fortran.symbol = " ";
+      fossil_branch.symbol = " ";
+      gcloud.symbol = " ";
+      git_branch.symbol = " ";
+      git_commit.tag_symbol = "  ";
+      golang.symbol = " ";
+      gradle.symbol = " ";
+      guix_shell.symbol = " ";
+      haskell.symbol = " ";
+      haxe.symbol = " ";
+      hg_branch.symbol = " ";
+      hostname.ssh_symbol = " ";
+      java.symbol = " ";
+      julia.symbol = " ";
+      kotlin.symbol = " ";
+      lua.symbol = " ";
+      memory_usage.symbol = "󰍛 ";
+      meson.symbol = "󰔷 ";
+      nim.symbol = "󰆥 ";
+      nix_shell.symbol = " ";
+      nodejs.symbol = " ";
+      ocaml.symbol = " ";
+      package.symbol = "󰏗 ";
+      perl.symbol = " ";
+      php.symbol = " ";
+      pijul_channel.symbol = " ";
+      pixi.symbol = "󰏗 ";
+      python.symbol = " ";
+      rlang.symbol = "󰟔 ";
+      ruby.symbol = " ";
+      rust.symbol = "󱘗 ";
+      scala.symbol = " ";
+      status.symbol = " ";
+      swift.symbol = " ";
+      xmake.symbol = " ";
+      zig.symbol = " ";
 
-      directory = {
-        style = "blue";
-      };
-
-      character = {
-        success_symbol = "[](purple) ";
-        error_symbol = "[](red) ";
-        vimcmd_symbol = "[](green) ";
-      };
-
-      git_branch = {
-        format = "[$branch]($style) ";
-        style = "bright-black";
-      };
-
-      git_status = {
-        format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style) ";
-        style = "cyan";
-        conflicted = "​";
-        untracked = "​";
-        modified = "​";
-        staged = "​";
-        renamed = "​";
-        deleted = "​";
-        stashed = "≡";
-      };
-
-      git_state = {
-        format = "([$state( $progress_current/$progress_total)]($style)) ";
-        style = "bright-black";
-      };
-
-      cmd_duration = {
-        format = "[$duration]($style) ";
-        style = "yellow";
-      };
-
-      python = {
-        format = "[$virtualenv]($style) ";
-        style = "bright-black";
+      os.symbols = {
+        Alpaquita = " ";
+        Alpine = " ";
+        AlmaLinux = " ";
+        Amazon = " ";
+        Android = " ";
+        AOSC = " ";
+        Arch = " ";
+        Artix = " ";
+        CachyOS = " ";
+        CentOS = " ";
+        Debian = " ";
+        DragonFly = " ";
+        Elementary = " ";
+        Emscripten = " ";
+        EndeavourOS = " ";
+        Fedora = " ";
+        FreeBSD = " ";
+        Garuda = "󰛓 ";
+        Gentoo = " ";
+        HardenedBSD = "󰞌 ";
+        Illumos = "󰈸 ";
+        Ios = "󰀷 ";
+        Kali = " ";
+        Linux = " ";
+        Mabox = " ";
+        Macos = " ";
+        Manjaro = " ";
+        Mariner = " ";
+        MidnightBSD = " ";
+        Mint = " ";
+        NetBSD = " ";
+        NixOS = " ";
+        Nobara = " ";
+        OpenBSD = "󰈺 ";
+        openSUSE = " ";
+        OracleLinux = "󰌷 ";
+        Pop = " ";
+        Raspbian = " ";
+        Redhat = " ";
+        RedHatEnterprise = " ";
+        RockyLinux = " ";
+        Redox = "󰀘 ";
+        Solus = "󰠳 ";
+        SUSE = " ";
+        Ubuntu = " ";
+        Unknown = " ";
+        Void = " ";
+        Windows = "󰍲 ";
+        Zorin = " ";
       };
     };
   };
