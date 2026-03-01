79297d0bbb717b6700e5a0de62792caf004cd76f
William-Ro <william.r2409@gmail.com>
2026-03-01 17:50:41 -0600

    feat(gnome): add no-overview

--- modules/linux/gnome-extensions.nix
+++ modules/linux/gnome-extensions.nix
@@ -6,7 +6,6 @@
     blur-my-shell # Modern blur effects
     vitals # System monitor in top bar
     clipboard-indicator # Clipboard manager
-    caffeine # Prevent auto-suspend
-    gsconnect # KDE Connect integration for phones
+    no-overview
   ];
 }
