0039b907eb753a71a06cd5afb4b269fb10d46699
William-Ro <william.r2409@gmail.com>
2026-03-01 21:52:36 -0600

    chore: cleanup audio module

--- modules/system/audio.nix
+++ modules/system/audio.nix
@@ -8,28 +8,5 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-
-    # Set proper sample rate (48kHz is standard for most audio)
-    extraConfig.pipewire."92-low-latency" = {
-      "context.properties" = {
-        "default.clock.rate" = 48000;
-        "default.clock.allowed-rates" = [
-          44100
-          48000
-        ];
-      };
-    };
-
-    # Allow volume above 100% to compensate for quiet speakers
-    wireplumber.extraConfig."51-increase-headroom" = {
-      "monitor.alsa.rules" = [
-        {
-          matches = [ { "node.name" = "~alsa_output.*"; } ];
-          actions.update-props = {
-            "session.suspend-timeout-seconds" = 0;
-          };
-        }
-      ];
-    };
   };
 }
