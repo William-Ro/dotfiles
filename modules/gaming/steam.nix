3ae20485b4a87b343ea16276b5ea8d7e9d95484c
William-Ro <william.r2409@gmail.com>
2026-03-01 22:14:24 -0600

    chore: use env variables for all steam games

--- modules/gaming/steam.nix
+++ modules/gaming/steam.nix
@@ -1,9 +1,22 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = false;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
+
+    package = pkgs.steam.override {
+      extraEnv = {
+        MANGOHUD = "1";
+        MANGOHUD_CONFIG = "read_cfg,no_display";
+        GAMEMODERUN = "1";
+        VKD3D_CONFIG = "dxr,dxr11";
+        PROTON_LOCAL_SHADER_CACHE = "1";
+        MESA_SHADER_CACHE_MAX_SIZE = "16G";
+        WINE_VK_VULKAN_ONLY = "1";
+        MESA_GLSL_CACHE_MAX_SIZE = "16G";
+      };
+    };
   };
 }
