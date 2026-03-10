b8fc87adeeb5a593bc13a5336918ea36eaa06cd8
William-Ro <william.r2409@gmail.com>
2026-03-09 19:28:31 -0600

    chore: add steam env variables for my desktop

--- modules/gaming/steam.nix
+++ modules/gaming/steam.nix
@@ -9,12 +9,15 @@
     package = pkgs.steam.override {
       extraEnv = {
         MANGOHUD = "1";
+        MANGOHUD_CONFIG = "no_display,fps,cpu_stats,cpu_temp,gpu_stats,gpu_temp";
         GAMEMODERUN = "1";
         VKD3D_CONFIG = "dxr,dxr11";
         PROTON_LOCAL_SHADER_CACHE = "1";
         MESA_SHADER_CACHE_MAX_SIZE = "16G";
-        WINE_VK_VULKAN_ONLY = "1";
         MESA_GLSL_CACHE_MAX_SIZE = "16G";
+        PROTON_ENABLE_NVAPI = "1"; 
+        __GL_SHADER_DISK_CACHE_SKIP_CLEANUP = "1";
+        __GL_SHADER_DISK_CACHE_SIZE = "17179869184";
       };
     };
   };
