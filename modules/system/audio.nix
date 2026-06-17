06fa415ad1280f42973b4554f836d121c9ccb852
William-Ro <william.r2409@gmail.com>
2026-06-16 20:38:09 -0600

    fix(audio): force analog stereo profile on onboard Realtek ALC1220

--- modules/system/audio.nix
+++ modules/system/audio.nix
@@ -8,5 +8,20 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    # Force analog stereo profile on the onboard Realtek ALC1220
+    wireplumber.extraConfig = {
+      "51-onboard-analog-profile" = {
+        "monitor.alsa.rules" = [
+          {
+            matches = [ { "device.name" = "alsa_card.pci-0000_0d_00.6"; } ];
+            actions = {
+              update-props = {
+                "device.profile" = "output:analog-stereo+input:analog-stereo";
+              };
+            };
+          }
+        ];
+      };
+    };
   };
 }
